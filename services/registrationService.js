1beab8d5589ee25d45d39ee2357d8adbc5deb6a3
Adedolapo-prog <shayne.doly94@gmail.com>
2022-02-02 14:00:28 +0100

    done with registration api for companies

--- services/registrationService.js
+++ services/registrationService.js
@@ -3,7 +3,10 @@ const { getHashTags } = require("../utils")
 
 const registrationService = async (body) => {
   try {
+    //getting the hashed tags using the getHashTags function
     let hashtags = await getHashTags(body.bestHeadline)
+
+    //instantiating a new registration object
     const newRegistration = new Registration({ ...body, hashtags })
     await newRegistration.save()
     return newRegistration
